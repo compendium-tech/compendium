6ce6807bcfca8cdac4cc48ffdabd99766300406a
abs0luty <adisalimgereev@gmail.com>
2025-07-17 00:39:52 +0500

    feat(user-service): add mocks for service types

--- user-service/internal/service/user.go
+++ user-service/internal/service/user.go
@@ -2,12 +2,14 @@ package service
 
 import "github.com/seacite-tech/compendium/user-service/internal/repository"
 
-type UserService struct {
+type UserService interface{}
+
+type userServiceImpl struct {
 	userRepository repository.UserRepository
 }
 
-func NewUserService(userRepository repository.UserRepository) *UserService {
-	return &UserService{
+func NewUserService(userRepository repository.UserRepository) *userServiceImpl {
+	return &userServiceImpl{
 		userRepository: userRepository,
 	}
 }
